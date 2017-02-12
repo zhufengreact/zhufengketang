cbb750b1c97a8a1c7076b6d2038bede21449780d
yinggeABC <906772533@qq.com>
2017-02-12 21:20:28 +0800

    首页轮播图

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,21 @@
  * Created by weimin on 17-2-12.
  */
 import React,{Component} from "react"
+import {Carousel} from "antd"
+
 export default class Home extends Component{
     render(){
         return (
-            <div>Home</div>
+            <div>
+             <Carousel autoplay>
+                 <div>
+                     <img src={require("../image/carous1.png")} alt="" style={{width:"100%"}}/>
+                 </div>
+                 <div>
+                     <img src={require("../image/carous2.png")} alt="" style={{width:"100%"}}/>
+                 </div>
+             </Carousel>
+            </div>
         )
     }
 }
