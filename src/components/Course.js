59dea666d09c05499f948def04f7668badd296ed
yinggeABC <906772533@qq.com>
2017-02-16 19:29:32 +0800

    finish empty page of course

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,10 +2,19 @@
  * Created by weimin on 17-2-12.
  */
 import React,{Component} from "react"
+import {Button,Row,Col} from "antd"
+
 export default class Course extends Component{
+    handleClick(){
+        this.props.history.push("/")
+    }
     render(){
         return (
-            <div>Course</div>
+            <div className="course-empty">
+                <div className="empty-hint"><img src=""/></div>
+                <p className="empty-hint">您还没有选课，快去选课吧！</p>
+                <Button type='primary' size="large" className="empty-hint" onClick={this.handleClick.bind(this)}>去发现</Button>
+            </div>
         )
     }
 }
